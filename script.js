3fa45aa42130f1136f18101066768d4cd80b36a2
1mmanuelKant <joselisboaa05@gmail.com>
2022-07-30 23:56:48 -0300

    assets added and script changed

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ const city = document.querySelector('.city-name');
 form.addEventListener('submit', event => {
   event.preventDefault();
   SubmitWeatherForm();
+  form.reset()
 })
 
 function weatherResult(city, apiKey) {
@@ -16,16 +17,30 @@ async function SubmitWeatherForm() {
   try {
     const weatherResponse = await weatherResult(city.value, apiKey);
     const weatherData = await weatherResponse.json();
+    
+    const cardContainer = document.createElement('div');
+    cardContainer.classList.add('card-container');
+    const cityName = document.createElement('div');
+    cityName.classList.add('city-name');
     const weatherCard = document.createElement('div');
-    weatherCard.classList.add('weather-city')
-    const countryName = document.createElement('div');
-    countryName.classList.add('country')
+    weatherCard.classList.add('weather-city');
     const cityTemp = document.createElement('div');
-    cityTemp.classList.add('temp')
+    cityTemp.classList.add('temp');
     const tempIcon = document.createElement('img');
-    tempIcon.classList.add('icon')
-    countryName.innerHTML = `País: ${weatherData.sys.country}`
-    cityTemp.innerHTML = `Temperatura Atual: ${weatherData.main.temp}°C`
+    tempIcon.classList.add('icon');
+    const infos = document.createElement('div');
+    infos.classList.add('weather-infos');
+
+    cityName.innerHTML = `${weatherData.name} <sup>${weatherData.sys.country}</sup>`
+    cityTemp.innerHTML = `${weatherData.main.temp}<sup>°C</sup>`
+    tempIcon.setAttribute('src', `./assets/${weatherData.weather[0].icon}.svg`);
+    
+
+    container.appendChild(cardContainer);
+    cardContainer.appendChild(infos);
+    infos.appendChild(cityName);
+    infos.appendChild(cityTemp);
+    infos.appendChild(tempIcon)
     console.log(weatherData)
   } catch (error) {
     console.log(error);
