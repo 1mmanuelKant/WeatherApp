abf81132535b434af866f0f19b7401faf2d05870
1mmanuelKant <joselisboaa05@gmail.com>
2022-07-31 23:27:58 -0300

    general infos added (max/min temp, pressure, wind velocity...)

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 const apiKey = '77245b658e8beb7b14d00f090d9ded4f';
-const container = document.querySelector('.container')
+const container = document.querySelector('.container');
 const form = document.querySelector('#weather-form');
 const city = document.querySelector('.city-name');
 
 form.addEventListener('submit', event => {
   event.preventDefault();
   SubmitWeatherForm();
-  form.reset()
+  form.reset();
 })
 
 function weatherResult(city, apiKey) {
@@ -17,6 +17,18 @@ async function SubmitWeatherForm() {
   try {
     const weatherResponse = await weatherResult(city.value, apiKey);
     const weatherData = await weatherResponse.json();
+    const sunriseSec = weatherData.sys.sunrise + weatherData.timezone;
+    const sunsetSec = weatherData.sys.sunset + weatherData.timezone;
+    const dateSunset = new Date(sunsetSec * 1000);
+    const dateSunrise = new Date(sunriseSec * 1000);
+    const dateSunriseUTC = dateSunrise.toUTCString();
+    const dateSunsetUTC = dateSunset.toUTCString();
+
+    console.log(dateSunriseUTC, dateSunsetUTC);
+    const timeStringSunset = dateSunsetUTC[17] + dateSunsetUTC[18] + dateSunsetUTC[19] + dateSunsetUTC[20] + dateSunsetUTC[21];
+    const timeStringSunrise = dateSunriseUTC[17] + dateSunriseUTC[18] + dateSunriseUTC[19] + dateSunriseUTC[20] + dateSunriseUTC[21];
+
+    console.log(dateSunrise , timeStringSunrise, timeStringSunset);
     
     const cardContainer = document.createElement('div');
     cardContainer.classList.add('card-container');
@@ -28,19 +40,54 @@ async function SubmitWeatherForm() {
     cityTemp.classList.add('temp');
     const tempIcon = document.createElement('img');
     tempIcon.classList.add('icon');
-    const infos = document.createElement('div');
-    infos.classList.add('weather-infos');
+    const description = document.createElement('div');
+    description.classList.add('wheater-description');
+    const weatherInfos = document.createElement('div');
+    weatherInfos.classList.add('weather-infos');
+    const generalInfos = document.createElement('div');
+    generalInfos.classList.add('general-infos');
+    const humidity = document.createElement('div');
+    humidity.classList.add('humidity');
+    const minMax = document.createElement('div');
+    minMax.classList.add('minmax-temp');
+    const pressure = document.createElement('div');
+    pressure.classList.add('pressure');
+    const wind = document.createElement('div');
+    wind.classList.add('wind-velocity');
+    const sunrise = document.createElement('div');
+    sunrise.classList.add('city-sunrise');
+    const sunset = document.createElement('div');
+    sunset.classList.add('city-sunset');
+    const feelsLike = document.createElement('div');
+    feelsLike.classList.add('temp-feelslike');
 
-    cityName.innerHTML = `${weatherData.name} <sup>${weatherData.sys.country}</sup>`
-    cityTemp.innerHTML = `${weatherData.main.temp}<sup>°C</sup>`
+    cityName.innerHTML = `${weatherData.name} <sup>${weatherData.sys.country}</sup>`;
+    cityTemp.innerHTML = `${Math.round(weatherData.main.temp)}<sup>°C</sup>`;
     tempIcon.setAttribute('src', `./assets/${weatherData.weather[0].icon}.svg`);
-    
+    description.innerHTML = `<span>${weatherData.weather[0].description}</span>`;
+    humidity.innerHTML = `<img src="./assets/humidity.svg"/> <span>Umidade ${weatherData.main.humidity}%</span>`;
+    minMax.innerHTML = `<img src="./assets/minmax.svg"/> <span>Max./Mín. ${Math.round(weatherData.main.temp_max)}°/${Math.round(weatherData.main.temp_min)}°</span>`;
+    pressure.innerHTML = `<img src="./assets/pressure.svg"/> <span>Pressão ${weatherData.main.pressure} Pa</span>`;
+    wind.innerHTML = `<img src="./assets/wind.svg"/> <span>Vento ${weatherData.wind.speed * 3.6} km/h</span>`;
+    sunrise.innerHTML = `<img src="./assets/sunrise.svg"/> <span>Nascer do Sol: ${timeStringSunrise}h</span>`;
+    sunset.innerHTML = `<img src="./assets/sunset.svg"/> <span>Por do Sol: ${timeStringSunset}h</span>`;
+    feelsLike.innerHTML = `<img src="./assets/feels-like.svg"/> <span>Sensação térmica: ${weatherData.main.feels_like}°C</span>`
 
     container.appendChild(cardContainer);
-    cardContainer.appendChild(infos);
-    infos.appendChild(cityName);
-    infos.appendChild(cityTemp);
-    infos.appendChild(tempIcon)
+    container.appendChild(generalInfos);
+    cardContainer.appendChild(weatherInfos);
+    weatherInfos.appendChild(cityName);
+    weatherInfos.appendChild(cityTemp);
+    weatherInfos.appendChild(tempIcon);
+    weatherInfos.appendChild(description);
+    generalInfos.appendChild(feelsLike);
+    generalInfos.appendChild(sunrise);
+    generalInfos.appendChild(sunset);
+    generalInfos.appendChild(minMax);
+    generalInfos.appendChild(humidity);
+    generalInfos.appendChild(pressure);
+    generalInfos.appendChild(wind);
+
     console.log(weatherData)
   } catch (error) {
     console.log(error);
